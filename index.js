d86b5a943ba20cfefdffbee939f9328c6bcaae2a
James Sharpe <jameswrightsharpe@gmail.com>
2019-10-18 09:28:29 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function BoardMember(name, homeState, training) {
+  this.name = name;
+  this.homeState = homeState;
+  this.training = training;
+}
+
+BoardMember.prototype.veto = () => "No, I must disagree";
+BoardMember.prototype.approve = () => "You can do that!";
+BoardMember.prototype.doCharity = () => "I like to help people.";
+BoardMember.prototype.releasePressStatement = () =>
+  "You will see great things from Scuber.";
+BoardMember.prototype.sayHi = function() {
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
+};
