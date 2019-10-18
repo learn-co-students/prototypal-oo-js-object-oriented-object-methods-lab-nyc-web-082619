d71873f1455ca306a4f3560e8b7bcd1509dd1d38
James Sharpe <jameswrightsharpe@gmail.com>
2019-10-18 09:28:43 -0400

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ BoardMember.prototype.approve = () => "You can do that!";
 BoardMember.prototype.doCharity = () => "I like to help people.";
 BoardMember.prototype.releasePressStatement = () =>
   "You will see great things from Scuber.";
-BoardMember.prototype.sayHi = function() {
+BoardMember.prototype.sayHi = () => {
   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
 };
